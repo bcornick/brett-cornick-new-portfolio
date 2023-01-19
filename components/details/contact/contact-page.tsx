f0e712b706908482f70721893da9bcf35c7392b6
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-01-19 09:32:47 -0700

    refactoring

--- components/details/contact/contact-page.tsx
+++ components/details/contact/contact-page.tsx
@@ -1,30 +1,12 @@
 import SocialBox from './social-box';
+import socials from '../../../content/socials';
 import classes from './contact-page.module.css';
-
-const socialArray = [
-  {
-    title: 'linkedin',
-    address: 'https://www.linkedin.com/in/brettcornick/',
-  },
-  {
-    title: 'twitter',
-    address: 'https://twitter.com/brett_cornick',
-  },
-  {
-    title: 'github',
-    address: 'https://github.com/bcornick',
-  },
-  {
-    title: 'medium',
-    address: 'https://medium.com/@brett.cornick',
-  },
-];
 
 const ContactPage = (): JSX.Element => {
   return (
     <div className={classes.contactContainer}>
       <div className={classes.socialsContainer}>
-        {socialArray.map((social, i) => (
+        {socials.map((social, i) => (
           <SocialBox
             key={i}
             social={social}
---
+++ content/socials.tsx
@@ -0,0 +1,20 @@
+const socials = [
+  {
+    title: 'linkedin',
+    address: 'https://www.linkedin.com/in/brettcornick/',
+  },
+  {
+    title: 'twitter',
+    address: 'https://twitter.com/brett_cornick',
+  },
+  {
+    title: 'github',
+    address: 'https://github.com/bcornick',
+  },
+  {
+    title: 'medium',
+    address: 'https://medium.com/@brett.cornick',
+  },
+];
+
+export default socials;
