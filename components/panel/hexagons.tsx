54051b42bd6685a7eee17ad105e4247a786f35b1
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-01-20 12:31:09 -0700

    minor style changes

--- components/panel/hexagons.tsx
+++ components/panel/hexagons.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import hexagon from '../../public/site/hexagon.png';
+import hexagon from '../../public/site/hex-pink.png';
 import classes from './hexagons.module.css';
 
 const numHexes = 12;
